df8b8a4fbf7bd985f831364b97cdd6ad87dd8352
ivanchuranov <113765929+ivanchuranov@users.noreply.github.com>
2023-06-03 11:14:32 +0300

    11 - поправил базу данных

--- Models/initialDatabase.py
+++ Models/initialDatabase.py
@@ -1,44 +1,98 @@
 from peewee import *
 
 con = SqliteDatabase("data/NeBana.sqlite")
+
 class BaseModel(Model):
 
     class Meta:
         database = con
 
 class Roles(BaseModel):
-    id = AutoField(column_name="RoleId")
-    rolename = TextField(column_name="RoleName", null=False)
+    id = AutoField(column_name="id")
+    rolename = TextField(column_name="name", null=False)
     class Meta:
         table_name = "Roles"
+
 Roles.create_table()
 
-class Procedures (BaseModel):
-    id = AutoField(column_name="ProceduresId")
-    name = TextField(column_name="ProceduresName", null=False)
+class Procedures(BaseModel):
+    id = AutoField(column_name="id")
+    name = TextField(column_name="name", null=False)
+    description = TextField(column_name="description", null=True)
+    price = IntegerField(column_name="price",null=False)
+
     class Meta:
-        table_name = "Procedures "
+        table_name = "Procedures"
+
 Procedures.create_table()
 
-class SpecialOffers (BaseModel):
-    id = AutoField(column_name="SpecialOffersId")
-    name = TextField(column_name="SpecialOffersName", null=False)
+class Sales(BaseModel):
+    id = AutoField(column_name="id")
+    procedure = ForeignKeyField(Procedures, on_delete="cascade", on_update="cascade", to_field="id")
+    price = IntegerField(column_name="price", null=False)
+    endDate = DateTimeField(column_name="endDate", null=False)
+    malingDate = DateTimeField(column_name="malingDate", null=False)
+
     class Meta:
-        table_name = "SpecialOffers "
+        table_name = "Sales"
+
+Sales.create_table()
+
+class SpecialOffers(BaseModel):
+    id = AutoField(column_name="id")
+    name = TextField(column_name="name", null=False)
+    description = TextField(column_name="description", null=True)
+    price = IntegerField(column_name="price", null=False)
+    endDate = DateTimeField(column_name="endDate",null=False)
+    malingDate = DateTimeField(column_name="malingDate",null=False)
+
+    class Meta:
+        table_name = "SpecialOffers"
+
 SpecialOffers.create_table()
 
-class User(BaseModel):
-    id = AutoField(column_name="UserId")
-    chat_id = IntegerField(column_name="ChatId", null=False)
-    is_bot = TextField(column_name="is_bot", null=False)
-    first_name = TextField(column_name="first_name", null=False)
-    last_name = TextField(column_name="last_name", null=False)
-    username = TextField(column_name="username", null=True)
-    language_code = TextField(column_name="language_code", null=False)
-    roleid = ForeignKeyField(Roles, on_delete="cascade", on_update="cascade", to_field="id")
+class SpecialOffersProcedures(BaseModel):
+    specialOffer = ForeignKeyField(SpecialOffers, on_delete="cascade", on_update="cascade", to_field="id")
+    procedure = ForeignKeyField(Procedures, on_delete="cascade", on_update="cascade", to_field="id")
+
     class Meta:
-        table_name = "User"
-User.create_table()
+        table_name = "SpecialOffersProcedures"
+
+SpecialOffersProcedures.create_table()
+
+class Users(BaseModel):
+    id = AutoField(column_name="id")
+    chat_id = IntegerField(column_name="chatId", null=False)
+    is_bot = TextField(column_name="isBot", null=False)
+    first_name = TextField(column_name="firstName", null=False)
+    last_name = TextField(column_name="lastName", null=False)
+    username = TextField(column_name="username", null=True)
+    language_code = TextField(column_name="languageCode", null=False)
+    role = ForeignKeyField(Roles, on_delete="cascade", on_update="cascade", to_field="id")
+    class Meta:
+        table_name = "Users"
+
+Users.create_table()
+
+class Orders(BaseModel):
+    id = AutoField(column_name="id")
+    total = IntegerField(column_name="total", null=False)
+    user = ForeignKeyField(Users, on_delete="cascade", on_update="cascade", to_field="id")
+    date = DateTimeField(column_name="date", null=True)
+    comment = TextField(column_name="comment",null=True)
+
+    class Meta:
+        table_name = "Orders"
+
+Orders.create_table()
+
+class OrdersProcedures(BaseModel):
+    order = ForeignKeyField(Orders, on_delete="cascade", on_update="cascade", to_field="id")
+    procedure = ForeignKeyField(Procedures, on_delete="cascade", on_update="cascade", to_field="id")
+    class Meta:
+        table_name = "OrdersProcedures"
+
+OrdersProcedures.create_table()
 
 if __name__ == '__main__':
     ''' role1 = Roles.create(rolename="admin")
