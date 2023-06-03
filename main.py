ab2207c04eafd11706166bd719a1ba23dea9d3f5
ivanchuranov <113765929+ivanchuranov@users.noreply.github.com>
2023-06-03 10:15:41 +0300

    8 - переделал под MVC

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from peewee import *
-from initialDatabase import *
+from Models.initialDatabase import *
 from telebot_router import TeleBot
-from cache import *
+from Services.StorageService import StorageService
 
 
 app = TeleBot("bot")
 
-
 @app.route('(?!/).+')
 def message_help(context):
-    chat_id = context["chat"]["id"]
+    user = StorageService.GetUserByContext(context)
     text = context['text']
-    app.send_message(chat_id, text)
+    app.send_message(user.chat_id, text)
 
-    user = GetInstance(chat_id)
-    if user == None:
-        first_name = context['from']["first_name"]
-        last_name = context['from']["last_name"]
-        username = context['from']["username"]
-        language_code = context['from']["language_code"]
-
-        user = User.create(chat_id= chat_id, is_bot= True, first_name= first_name, last_name= last_name, username= username, language_code =language_code, roleid= 2)
-        user.save()
 
 with open("bot.auth", 'r') as file:
     token = file.readline()
